895920921d2e01bd373b8db870a75d716a708828
massu-159 <magj.dada.37458@softbank.ne.jp>
2021-03-15 21:43:25 +0900

    first commit

---
+++ EC/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::name('product.')->group(function(){
+    Route::get('/', 'ProductController@index')->name('index');
+    Route::get('/product/{id}', 'ProductController@show')->name('show');
+});
+
+Route::name('line_item.')->group(function(){
+    Route::post('/line_item/create', 'LineItemController@create')->name('create');
+});
